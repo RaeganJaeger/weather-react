7f6e7c3b7c616fc4863892b81e17d09a975745e8
RaeganJaeger <rjaeger4@gmail.com>
2022-04-13 16:30:45 -0700

    debugging - functions still not working properly

--- src/Header.js
+++ src/Header.js
@@ -1,66 +1,64 @@
 export default function Header() {
   return (
-    <container>
-      <div className="header">
-        <h1 id="city-header">
-          <span className="city" id="city">
-            Minneapolis
-          </span>
-          , <span className="country" id="country"></span>
-        </h1>
-        <div className="today-date" id="today-date">
-          Sunday, March 27, 2022 | 19:33
+    <div className="header">
+      <h1 id="city-header">
+        <span className="city" id="city">
+          Minneapolis
+        </span>
+        , <span className="country" id="country"></span>
+      </h1>
+      <div className="today-date" id="today-date">
+        Sunday, March 27, 2022 | 19:33
+      </div>
+      <div className="row today">
+        <div className="col-4">
+          <img
+            src="https://i.pinimg.com/474x/84/a1/6a/84a16af6971e5c4234fa132c06b14969.jpg"
+            id="main-icon"
+            className="main-icon"
+            alt="img of sun"
+          />
         </div>
-        <div className="row today">
-          <div className="col-4">
-            <img
-              src="https://i.pinimg.com/474x/84/a1/6a/84a16af6971e5c4234fa132c06b14969.jpg"
-              id="main-icon"
-              className="main-icon"
-              alt="img of sun"
-            />
-          </div>
-          <div className="col-6">
-            <span className="today-temp" id="today-temp">
-              55
-            </span>{" "}
-            °
-            <span id="degreeUnit" className="degreeUnit">
-              F
-            </span>
-            <ul className="today-summary">
-              <li>
-                <a
-                  href="https://epic-colden-a184c0.netlify.app/"
-                  id="fahrenheit-link"
-                  className="active"
-                >
-                  F
-                </a>{" "}
-                |{" "}
-                <a
-                  href="https://epic-colden-a184c0.netlify.app/"
-                  id="celsius-link"
-                >
-                  C
-                </a>
-              </li>
-              <li>
-                <strong>Today:</strong>{" "}
-                <span id="description">
-                  Cloudy with a 20% chance of perception
-                </span>
-              </li>
-              <li>
-                <strong>Humidity:</strong> <span id="humidity">52</span>%
-              </li>
-              <li>
-                <strong>Wind:</strong> <span id="wind">1</span> mph
-              </li>
-            </ul>
-          </div>
+        <div className="col-6">
+          <span className="today-temp" id="today-temp">
+            55
+          </span>{" "}
+          °
+          <span id="degreeUnit" className="degreeUnit">
+            F
+          </span>
+          <ul className="today-summary">
+            <li>
+              <a
+                href="https://epic-colden-a184c0.netlify.app/"
+                id="fahrenheit-link"
+                className="active"
+              >
+                F
+              </a>{" "}
+              |{" "}
+              <a
+                href="https://epic-colden-a184c0.netlify.app/"
+                id="celsius-link"
+              >
+                C
+              </a>
+            </li>
+            <li>
+              <strong>Today:</strong>{" "}
+              <span id="description">
+                Cloudy with a 20% chance of perception
+              </span>
+            </li>
+            <li>
+              <strong>Humidity:</strong> <span id="humidity">52</span>%
+            </li>
+            <li>
+              <strong>Wind:</strong> <span id="wind">1</span> mph
+            </li>
+          </ul>
         </div>
       </div>
-    </container>
+    </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,2 +1,63 @@
-//import React, { useState } from "react";
-//import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search(props) {
+  const [city, setCity] = useState("Minneapolis");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("");
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temp: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "fc4cce9256dbfa5057522b9ccaf07872";
+    let units = "imperial";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        onChange={updateCity}
+        placeholder="Enter a city..."
+      />{" "}
+      <button type="submit" value="Search">
+        Search
+      </button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temp)}° F</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity} %</li>
+          <li>Wind: {weather.wind} mph</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>{form}</div>;
+  }
+}
